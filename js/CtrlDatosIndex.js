d5e89888ee44b62541dea4535ce5414ecb397f30
proyectorca <proyectofinalca@gmail.com>
2021-12-12 15:30:25 -0600

    indextotal

--- js/CtrlDatosIndex.js
+++ js/CtrlDatosIndex.js
@@ -14,12 +14,63 @@ import {
   } from "./seguridad.js";
 
 /**
- 
+Sesion
  */
+import {
+  iniciaSesión,
+  terminaSesión
+} from "./seguridad.js";
+
 /** @type {HTMLFormElement} */
 const forma = document["forma"];
+/** @type {HTMLImageElement} */
+const avatar = document.
+  querySelector("#avatar");
 
-  
+/* Escucha cambios de usuario.
+ * El primer parámetro es una
+ * función que se invoca cada que
+ * hay un cambio de usuario y
+ * recibe los datos del usuario.
+ * El segundo parámetro es una
+ * función que se invoca cuando se
+ * presenta un error en un cambio
+ * de usuario y recibe un Error.
+ */
+getAuth().onAuthStateChanged(
+  muestraSesión, muestraError);
+
+/** Muestra los datos del usuario
+ * o manda a iniciar sesión en
+ * caso de que no haya empezado.
+ * @param {import(
+    "../lib/tiposFire").
+    User} usuario modelo con las
+ *    características del usuario
+ *    o null si no ha iniciado
+ *    sesión. */
+async function
+  muestraSesión(usuario) {
+  if (usuario && usuario.email) {
+    // Usuario aceptado.
+    forma.email.value =
+      usuario.email || "";
+    forma.nombre.value =
+      usuario.displayName || "";
+    avatar.src =
+      usuario.photoURL || "";
+
+    forma.terminarSesión.
+      addEventListener(
+        "click", terminaSesión);
+  } else {
+    // No ha iniciado sesión.
+    iniciaSesión();
+  }
+}
+
+/*----------------------------*/
+
   /** @type {HTMLUListElement} */
   // @ts-ignore
   const datos = document.
@@ -59,9 +110,14 @@ const forma = document["forma"];
     if (snap.size > 0) {
       /** @type {
             Promise<string>[]} */
+     
       let usuarios = [];
-      snap.forEach(doc => forma.email.
+      snap.forEach(doc => 
+        
+        usuarios.
         push(htmlFila(doc)));
+
+        
       const htmlFilas =
         await Promise.all(usuarios);
       /* Junta el todos los
