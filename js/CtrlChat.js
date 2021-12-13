93b11c45681f69d88876b7d9daba4e749ed14e3b
proyectorca <proyectofinalca@gmail.com>
2021-12-12 20:25:09 -0600

    chat

--- js/CtrlChat.js
+++ js/CtrlChat.js
@@ -28,7 +28,7 @@ getAuth().onAuthStateChanged(
     usuario */
 async function protege(usuario) {
   if (tieneRol(usuario,
-    ["Cliente"])) {
+    ["Encargado"])) {
     usuarioId = usuario.email;
     consulta();
     forma.addEventListener(
