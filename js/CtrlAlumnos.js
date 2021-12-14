f02413a22f4362622903022baf56608c8c74e128
proyectorca <proyectofinalca@gmail.com>
2021-12-14 16:33:37 -0600

    a

--- js/CtrlAlumnos.js
+++ js/CtrlAlumnos.js
@@ -78,12 +78,12 @@ function htmlFila(doc) {
   parámetros.append("id", doc.id);
   return ( /* html */
     `<li>
-      <a class="fila" href=
+      <p class="fila" href=
   "alumno.html?${parámetros}">
         <strong class="primario">
           ${matricula} ${nombre} ${dformat}
         </strong>
-      </a>
+      </p>
      
     </li>`);
 }
