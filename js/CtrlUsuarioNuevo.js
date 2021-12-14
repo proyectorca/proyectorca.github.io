27bc0e5cc54f9727800a588967acac5d895df777
proyectorca <proyectofinalca@gmail.com>
2021-12-13 19:09:54 -0600

    new

--- js/CtrlUsuarioNuevo.js
+++ js/CtrlUsuarioNuevo.js
@@ -11,7 +11,7 @@ import {
 import {
   checksRoles,
   guardaUsuario,
-  checksAlumno
+  selectAlumnos
 } from "./usuarios.js";
 
 /** @type {HTMLFormElement} */
@@ -19,9 +19,6 @@ const forma = document["forma"];
 /** @type {HTMLUListElement} */
 const listaRoles = document.
   querySelector("#listaRoles");
-const listaAlumnos = document.
-  querySelector("#listaAlumnos");
-  
 
 getAuth().onAuthStateChanged(
   protege, muestraError);
